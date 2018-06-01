db305d6595fda107a7ad1151ebc83a5d99952ef1
ssssseason <3150102305@zju.edu.cn>
2018-06-01 21:19:01 +0800

    Update some test cases, and runtime function.

--- lib/tigerMain.s
+++ lib/tigerMain.s
@@ -1,118 +1,128 @@
-	# .file	1 "tigerMain.c"
-	# .section .mdebug.abi32
-	# .previous
-	# .gnu_attribute 4, 1
-	# .abicalls
-	# .option	pic0
-	.data
-	# .align	2
-$LC0:
-	.ascii	"test\012\000"
-	.text
-	# .align	2
-	.globl	tigerMain
-	# .set	nomips16
-	# .ent	tigerMain
-	# .type	tigerMain, @function
+.data
+.align 2
+L7:
+.word 2
+.ascii "\n"
+.align 2
+L6:
+.word 2
+.ascii "\n"
+.align 2
+L5:
+.word 2
+.ascii "\n"
+.align 2
+L1:
+.word 14
+.ascii "Hello World!\n"
+.text
+.align 2
+.globl tigerMain
 tigerMain:
-	# .frame	$fp,48,$31		# vars= 16, regs= 2/0, args= 16, gp= 8
-	# .mask	0xc0000000,-4
-	# .fmask	0x00000000,0
-	# .set	noreorder
-	# .set	nomacro
-	addiu	$sp,$sp,-48
-	sw	$31,44($sp)
-	sw	$fp,40($sp)
-	move	$fp,$sp
-	# lui	$2,%hi($LC0)
-	# addiu	$2,$2,%lo($LC0)
-	la $2, $LC0
-	sw	$2,24($fp)
-	jal	tigerGetchar
-	nop
+tigerMain_FRAMESIZE = 52
+addi $sp,$sp,-52
 
-	sw	$2,28($fp)
-	li	$4,97			# 0x61
-	jal	chr
-	nop
+L9:
+sw $ra, -4+tigerMain_FRAMESIZE($sp)
+sw $s7, -8+tigerMain_FRAMESIZE($sp)
+sw $s6, -12+tigerMain_FRAMESIZE($sp)
+sw $s5, -16+tigerMain_FRAMESIZE($sp)
+sw $s4, -20+tigerMain_FRAMESIZE($sp)
+sw $s3, -24+tigerMain_FRAMESIZE($sp)
+sw $s2, -28+tigerMain_FRAMESIZE($sp)
+sw $s1, -32+tigerMain_FRAMESIZE($sp)
+sw $s0, -36+tigerMain_FRAMESIZE($sp)
+la $t0, L1
+move $s0, $t0
+jal tigerGetchar
+nop
+move $s1, $v0
+move $s2, $s0
+li $t0, 0
+li $t0, 3
+li $s3, 0
+move $s2, $t0
+ble $s3, $s2, L3
+nop
 
-	sw	$2,32($fp)
-	li	$4,98			# 0x62
-	jal	chr
-	nop
+L2:
+move $a0, $s0
+move $a1, $s1
+jal concat
+nop
+move $s2, $v0
+move $a0, $s2
+jal printStr
+nop
+la $t0, L5
+move $a0, $t0
+jal printStr
+nop
+move $a0, $s2
+jal size
+nop
+move $t0, $v0
+li $t1, 3
+addi $t0, $t0, -3
+move $a0, $s2
+move $a1, $t1
+move $a2, $t0
+jal substring
+nop
+move $t0, $v0
+move $a0, $t0
+jal printStr
+nop
+la $t0, L6
+move $a0, $t0
+jal printStr
+nop
+move $a0, $s0
+jal ord
+nop
+move $t0, $v0
+move $a0, $t0
+jal chr
+nop
+move $t0, $v0
+move $a0, $t0
+jal printStr
+nop
+la $t0, L7
+move $a0, $t0
+jal printStr
+nop
+li $v0, 0
+lw $s7, -8+tigerMain_FRAMESIZE($sp)
+lw $s6, -12+tigerMain_FRAMESIZE($sp)
+lw $s5, -16+tigerMain_FRAMESIZE($sp)
+lw $s4, -20+tigerMain_FRAMESIZE($sp)
+lw $s3, -24+tigerMain_FRAMESIZE($sp)
+lw $s2, -28+tigerMain_FRAMESIZE($sp)
+lw $s1, -32+tigerMain_FRAMESIZE($sp)
+lw $s0, -36+tigerMain_FRAMESIZE($sp)
+lw $ra, -4+tigerMain_FRAMESIZE($sp)
+j L8
+nop
+
+L3:
+move $a0, $s0
+jal printStr
+nop
+bge $s3, $s2, L2
+nop
+
+L4:
+addi $s3, $s3, 1
+j L3
+nop
+
+L8:
+nop
+
+addi $sp,$sp,52
+jr $ra
 
 
-	lw	$4,32($fp)
-	move	$5,$2
-	jal	concat
-	nop
 
-	move	$4,$2
-	lw	$5,28($fp)
-	jal	concat
-	nop
 
-	sw	$2,36($fp)
-	lw	$4,36($fp)
-	jal	printStr
-	nop
-
-	jal	flush
-	nop
-
-	lw	$4,36($fp)
-	li	$5,1			# 0x1
-	li	$6,1			# 0x1
-	jal	substring
-	nop
-
-	move	$4,$2
-	jal	printStr
-	nop
-
-	lw	$4,36($fp)
-	jal	size
-	nop
-
-	move	$4,$2
-	jal	printInt
-	nop
-
-	lw	$4,36($fp)
-	jal	size
-	nop
-
-	move	$4,$2
-	jal	not
-	nop
-
-	move	$4,$2
-	jal	printInt
-	nop
-
-	lw	$4,32($fp)
-	jal	ord
-	nop
-
-	move	$4,$2
-	jal	printInt
-	nop
-
-	not $t0, $t1
-
-	li	$4,-1			# 0xffffffffffffffff
-	jal	tigerExit
-	nop
-
-	move	$sp,$fp
-	lw	$31,44($sp)
-	lw	$fp,40($sp)
-	addiu	$sp,$sp,48
-	j	$31
-	nop
-
-	# .set	macro
-	# .set	reorder
-	# .end	tigerMain
-	# .size	tigerMain, .-tigerMain
-	# .ident	"GCC: (Debian 4.6.3-14) 4.6.3"
